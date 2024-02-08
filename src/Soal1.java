e41cd63ef90b9d57be2817342a34c457e5b874e1
JumantaraReqi20 <reqi.jumantara@gmail.com>
2024-02-09 06:59:55 +0700

    first commit

--- src/Soal1.java
+++ src/Soal1.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.soal1;
 
 /**
  * Program ini akan menjumlahkan dua buah data bertipe byte
@@ -12,7 +7,7 @@ package com.mycompany.soal1;
 public class Soal1 {
 
     public static void main(String[] args) {
-        byte angka1 = 115;
+        byte angka1 = 125;
         byte angka2 = 6;
         byte hasil = (byte) (angka1 + angka2);
         System.out.println("Hasil 1 = "+ hasil);
--- src/Soal2.java
+++ src/Soal2.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.soal2;
 
 /**
  * Program ini akan menampilkan string hasil operasi ternary bercabang
@@ -13,7 +8,7 @@ public class Soal2 {
 
     public static void main(String[] args) {
         int i = 42;
-            String s = (i<40) ? "Life" : (i>50) ? "universe" : "everything";
+        String s = (i<40) ? "Life" : (i>50) ? "universe" : "everything";
         System.out.println(s);
     }
 }
