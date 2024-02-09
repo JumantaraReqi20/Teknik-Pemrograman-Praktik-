c7d3839acaed886e03bab70b13d6b6b418ac413d
JumantaraReqi20 <reqi.jumantara@gmail.com>
2024-02-09 07:25:14 +0700

    add welcome

---
+++ src/Welcome.java
@@ -0,0 +1,15 @@
+public class Welcome {
+    /**
+     This program displays a greeting for the reader.
+     @version 1.30 2014-04-27
+     @author Cay Horstmann
+     */
+        public static void main(String[] args)
+        {
+            String greeting = "Welcome to Core Java!";
+            System.out.println(greeting);
+            for(int i = 0; i < greeting.length(); i++)
+                System.out.print("=");
+            System.out.println();
+        }
+    }
